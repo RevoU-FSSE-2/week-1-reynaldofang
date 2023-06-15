6f0c0aa2d6697f255a7ce6106192dc4a503d46bc
Reynaldo Fang <reynaldofang@hotmail.com>
2023-06-16 01:44:41 +0700

    pergantian nav, tambahan page contact dan perubahan pada link bisa ke tab lain

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 function sayHello() {
   alert("WELCOME TO MY WORLD!!~");
 }
+
+function submitForm() {
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var message = document.getElementById("message").value;
+
+  var alertMessage =
+    "Name: " + name + "\nEmail: " + email + "\nMessage: " + message;
+  alert(alertMessage);
+
+  /** Setelah melakukan submit maka value input tersebut tereset */
+  document.getElementById("name").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("message").value = "";
+}
